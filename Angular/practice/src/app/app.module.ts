54a7e2556f2dae42673ccf830360a0e8c89ea72a
Vijeth-V <vijeth1410@gmail.com>
2025-01-06 17:37:29 -0600

    final

--- Angular/practice/src/app/app.module.ts
+++ Angular/practice/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { TemplateDrivenFormsComponent } from './components/template-driven-forms/template-driven-forms.component';
 import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveCheckBoxComponent } from './components/reactive-check-box/reactive-check-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenFormsComponent,
     ReactiveFormsComponent,
+    ReactiveCheckBoxComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
--- Angular/practice/src/app/components/reactive-check-box/reactive-check-box.component.ts
+++ Angular/practice/src/app/components/reactive-check-box/reactive-check-box.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-check-box',
   standalone: false,
-  
   templateUrl: './reactive-check-box.component.html',
-  styleUrl: './reactive-check-box.component.scss'
+  styleUrls: ['./reactive-check-box.component.scss'],
 })
 export class ReactiveCheckBoxComponent {
+  movies = [
+    { id: 1, name: 'Avengers' },
+    { id: 2, name: 'Iron Man' },
+    { id: 3, name: 'Captain America' },
+    { id: 4, name: 'Spider Man' },
+    { id: 5, name: 'Black Panther' },
+  ];
 
+  // selectedMovies: { id: number; name: string }[] = [];
+
+  moviesForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.moviesForm = this.fb.group({
+      selectedMovies: [[]],
+    });
+  }
+
+  selectAll() {
+    const allMovies = this.movies.map((movie) => movie);
+    this.moviesForm.get('selectedMovies')?.setValue(allMovies);
+  }
+
+  clearAll() {
+    this.moviesForm.get('selectedMovies')?.setValue([]);
+  }
+
+  get selectedMovies() {
+    return this.moviesForm.get('selectedMovies')?.value || [];
+  }
+
+  onCheckboxChange(movie: any) {
+    const selectedMovies = this.selectedMovies;
+    if (selectedMovies.includes(movie)) {
+      const index = selectedMovies.indexOf(movie);
+      selectedMovies.splice(index, 1);
+    } else {
+      selectedMovies.push(movie);
+    }
+    this.moviesForm.get('selectedMovies')?.setValue(selectedMovies);
+  }
 }
